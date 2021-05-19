cf997636b36c9b2fb1ed07f841fc74341d77c9c4
144058 <144058@ust-global.com>
2021-05-19 13:37:25 +0530

    Skeleton creation
    Added database connectivity in application.properties file.

--- src/main/java/com/library/bookfinder/controller/MainController.java
+++ src/main/java/com/library/bookfinder/controller/MainController.java
@@ -4,9 +4,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/book")
 public class MainController {
 	@RequestMapping("/home")
 	public String showHome() {
-		return "Welcome Guest!";
+		return "Welcome User!";
 	}
 }
